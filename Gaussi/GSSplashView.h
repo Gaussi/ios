13fd8b62fb96854c99f0f06e02cc0c7a9ae9e108
Matt Sun <mattsun@wireless-230-52.uchicago.edu>
2014-04-17 20:14:20 -0500

    Part of image resources added. SplashView and ReaderView built.

---
+++ Gaussi/GSSplashView.h
@@ -0,0 +1,13 @@
+//
+//  GSSplashView.h
+//  Gaussi
+//
+//  Created by Matt Sun on 4/17/14.
+//  Copyright (c) 2014 Gaussi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSSplashView : UIViewController
+
+@end
